1d60976acc356f653c63933d120ce39c62e93ed1
Saad Khalid <saadkhalid913@gmail.com>
2022-08-16 13:57:46 -0400

    Added some testing

--- linkedlist.py
+++ linkedlist.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import Any, List, Dict, Set, Type, NoReturn
-
+from timeit import timeit
+import random
 class Node():
     def __init__(self, val: Any, prev: Node, next: Node) -> NoReturn:
         self.v: Any = val
@@ -11,8 +12,10 @@ class Node():
 class LinkedList():
     def __init__(self, values: List[Any]):
         self._root: Node = None
+        self.length: int = 0
         for val in values:
             self._insert(val)
+
 
     def __repr__(self) -> str:
         vals = []
@@ -28,6 +31,10 @@ class LinkedList():
         vals.append(current.v)
 
         return str(vals) 
+
+    def __len__(self) -> int:
+        return self.length
+            
     
     def _insert(self, val: Any) -> NoReturn:
         if not self._root:
@@ -39,11 +46,13 @@ class LinkedList():
                 current = next 
                 next = current.n
             current.n = Node(val, current, None)
+        self.length +=1 
 
     def addFirst(self, val: Any) -> NoReturn:
         newRoot = Node(val, None, self._root)
         self._root.p = newRoot
         self._root = newRoot
+        self.length +=1 
 
     def append(self, val: Any) -> NoReturn:
         self._insert(val)
@@ -53,6 +62,8 @@ class LinkedList():
             raise Exception("List is empty")
         removed = self._root
         self._root = removed.n
+        
+        self.length -=1 
         return removed.v
 
     def removeLast(self) -> Any:
@@ -68,6 +79,7 @@ class LinkedList():
         else:
             current.p.n = None 
         val = current.v
+        self.length -=1 
         return val
 
     def getLast(self) -> Any:
@@ -85,8 +97,105 @@ class LinkedList():
             return None
         else:
             return self._root.v
+            
+
+    def insert(self, val: Any, index: int) -> NoReturn:
+        if index == 0:
+            self.addFirst(val)
+        else:
+            assert index < len(self)
+            current_index = 1 
+            current = self._root
+            next = current.n
+            while (next != None and current_index < index):
+                current = next 
+                next = current.n
+                current_index +=1 
+            
+            # save old ith node 
+            temp = current.n
+
+            # add new node in front of current and make it point to the old ith node (now i + 1th node)
+            current.n = Node(val, current, temp)
+
+            self.length +=1 
+
+    def reverse(self) -> LinkedList:
+        if not self._root:
+            return self 
+        current = self._root 
+        next = self._root.n
+        self._root.n = None 
+        while (next != None):
+            temp = next.n
+            next.n = current 
+            current = next 
+            next = temp 
+        self._root = current 
+        
+        return self 
+
+    def __getitem__(self, index):
+        if not self._root or index >= len(self):
+            raise IndexError("Index out of bounds")
+        if index < 0:
+            return self[index % len(self)]
+        
+        i = 0
+        current = self._root
+        next = self._root.n
+        while i < index:
+            current = next 
+            next = current.n 
+            i +=1 
+        return current.v 
+
+    def getKthItemFromBack(self, k):
+        forwardIndex = 0
+        if not self._root:
+            return 
+
+        pointer = None 
+        next_pointer = None
+
+        current = self._root 
+        next = self._root.n
+        while next != None:
+            current = next 
+            next = current.n
+            if forwardIndex == ( k - 1 ):
+                pointer = self._root
+                next_pointer = pointer.n
+            forwardIndex +=1 
+            
+            if (pointer != None):
+                pointer = next_pointer
+                next_pointer = pointer.n
+            
+        return pointer.v 
 
 
-
+        
     
-L = LinkedList([1,2,3,4])
+num_trials = 10000 ## linear 
+num_items = 1000
+L = LinkedList([i for i in range(1, num_items)])
+
+def CreateList():
+    L = LinkedList([i for i in range(1, num_items)])
+
+def TestMoshMethod():
+    index = random.randint(1, num_items - 2)
+    kth_Item = (L.getKthItemFromBack(index))
+
+def TestMyMethod():
+    index = random.randint(1, num_items - 2)
+    kth_Item = (L[-index])
+
+
+print("Time to create array" ,timeit( CreateList, number = 1))
+print("Time for Mosh's Method",timeit( TestMoshMethod, number = num_trials), "sec")
+print("Time for my method", timeit(TestMyMethod, number = num_trials), "sec")
+
+
+
